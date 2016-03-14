e518ec4509fc50c6554755bffc0f162239449599
kickscar <kickscar@gmail.com>
2016-03-14 14:52:42 +0900

    1st commit

---
+++ src/com/estsoft/network/test/LocalHost.java
@@ -0,0 +1,33 @@
+package com.estsoft.network.test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class LocalHost {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String hostAddress = inetAddress.getHostAddress();
+			String hostname = inetAddress.getHostName();
+			
+			System.out.println( hostAddress );
+			System.out.println( hostname );
+			
+			byte[] addresses = inetAddress.getAddress();
+			for( int i = 0; i < addresses.length; i++ ) {
+				int address = addresses[i] & 0x000000ff;
+				System.out.print( address );
+				if( i < addresses.length - 1 ) {
+					System.out.print( "." );
+				}
+			}
+		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
