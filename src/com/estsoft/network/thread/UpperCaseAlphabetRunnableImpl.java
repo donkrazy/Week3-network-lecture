261adc03a679fc2139b61b8f8a815ba92914821b
kickscar <kickscar@gmail.com>
2016-03-15 11:31:24 +0900

    EchoServer 완성

---
+++ src/com/estsoft/network/thread/UpperCaseAlphabetRunnableImpl.java
@@ -0,0 +1,10 @@
+package com.estsoft.network.thread;
+
+public class UpperCaseAlphabetRunnableImpl extends UpperCaseAlphabet implements Runnable {
+
+	@Override
+	public void run() {
+		print();
+	}
+
+}
